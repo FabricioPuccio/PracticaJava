84ad32f88af5b7c9ce8afe2609d8515276a0529b
FabricioPuccio <fabriciopuccio@yahoo.com>
2023-09-28 11:32:08 -0300

    Práctica de colecciones

--- Practica/src/principal/Main.java
+++ Practica/src/principal/Main.java
@@ -1,12 +1,15 @@
 package principal;
 
 import ejercicios.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println(NumeroCapicua.numeroCapicua(null)); // false
+		/*System.out.println(NumeroCapicua.numeroCapicua(null)); // false
 		System.out.println(NumeroCapicua.numeroCapicua(123454321));// true
 		System.out.println(NumeroCapicua.numeroCapicua(-123454321));// true
 		System.out.println(NumeroCapicua.numeroCapicua(2));// true
@@ -18,7 +21,22 @@ public class Main {
 		System.out.println(Caramelolandia.pricioneroDulce(7, 4, 8));// 2
 		System.out.println(Caramelolandia.pricioneroDulce(1, 5, 2));// 1
 		System.out.println(Caramelolandia.pricioneroDulce(0, 10, 1));// 0
-		System.out.println(Caramelolandia.pricioneroDulce(455, 1585, 500));// 39
+		System.out.println(Caramelolandia.pricioneroDulce(455, 1585, 500));// 39*/
+		
+		
+		List<String> listaMedias = new ArrayList<String>(Arrays.asList("A","B","A","B","C","A","F","Z","C","H"));
+		System.out.println(UniversoParalelo.mediasAmigas(listaMedias));
+		//Resultado: [A, B, C]
+		listaMedias = Arrays.asList("A", "B", "C", "D", "A", "C", "D", "A");
+		System.out.println(UniversoParalelo.mediasAmigas(listaMedias));
+		//Resultado: [A, C, D]
+		listaMedias = Arrays.asList("R", "R", "A", "A", "S", "S", "G", "H");
+		System.out.println(UniversoParalelo.mediasAmigas(listaMedias));
+		//Resultado: [A, R, S]
+		listaMedias = Arrays.asList("R", "E", "T", "A", "P", "S", "G", "H");
+		System.out.println(UniversoParalelo.mediasAmigas(listaMedias));
+		//Resultado: []
+		
 
 	}
 
